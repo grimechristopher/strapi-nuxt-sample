b21c3f2fe29fcabbcacfe050b99b5d1486834f42
Christopher Grime <54651065+grimechristopher@users.noreply.github.com>
2023-03-17 21:16:22 -0700

    Add a carousel for slider component

--- frontend/components/MediaSlider.vue
+++ frontend/components/MediaSlider.vue
@@ -1,10 +1,22 @@
 <template>
-  <div v-for="file in files">
-    <img :src="getStrapiMedia(file.attributes.url)" height="100" />
+  <div>
+    <Carousel :items-to-show="1.5">
+      <Slide v-for="file in files" :key="file.id">
+        <img :src="getStrapiMedia(file.attributes.url)" />
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
 const props = defineProps({
   files: {
     type: Array,
